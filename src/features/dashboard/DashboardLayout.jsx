2ad4e0f0c2db7c8348e74b1b8fcf48729e94aa38
sameed02 <sameedahmad3@gmail.com>
2024-02-25 15:48:24 +0530

    custom hook useRecentStays & useRecentBookings to compute no of stays and no of bookings

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings.js";
+import Spinner from "../../ui/Spinner.jsx";
+import { useRecentStays } from "./useRecentStays.js";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,10 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isloadingStays } = useRecentStays();
+  if (isLoading || isloadingStays) return <Spinner />;
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>Stats</div>
